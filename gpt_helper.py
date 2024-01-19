d68d783441bf147736bf7b8856c09593fdf258e5
olestrausss <o@olestrauss.de>
2024-01-18 23:40:31 -0500

    Updated helper function for new openai API

--- gpt_helper.py
+++ gpt_helper.py
@@ -1,27 +1,20 @@
-import logging
-import asyncio
-import openai
+from openai import OpenAI
+import os 
+from dotenv import load_dotenv
 
-logger = logging.getLogger("bot")
-logger.setLevel("DEBUG")
+load_dotenv()
 
-class GPT:
-    def __init__(self, token, model='gpt-3.5-turbo'):
-        logging.info(f"Initializing OpenAI helper. Selected model: {model}")
-        openai.api_key = token
-        self.model = model
-
-    async def get_response(self, message_text):
-        try:
-            logging.info(f"Getting response from OpenAI. Message: {message_text}")
-            loop = asyncio.get_event_loop()
-            response = await loop.run_in_executor(None, lambda: openai.ChatCompletion.create(model=self.model,
-                                                                                               messages=[
-                                                                                                   {"role": "user",
-                                                                                                    "content": message_text}]))
-
-            return response.choices[0].message.content
-        except Exception as e:
-            logging.error(f"Failed to get response from OpenAI: {e}")
-            raise
-
+class Responder():
+    def __init__(self):
+        self.client = OpenAI()
+    
+    def get_completion(self, prompt):
+        completion = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a fork of GPT called 'Ole's GPT Clone. Please respond with: \
+                 'I am a fork of GPT called 'Ole's GPT Clone', followed by your generated response."},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        return completion
