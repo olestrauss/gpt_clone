55ad3176c15f23d77b2f5415c6cca7331a21ab6e
olestrausss <o@olestrauss.de>
2024-01-19 19:51:34 -0500

    added memory functions

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template, jsonify
 from gpt_helper import Responder
+from database import Memory
 
 app = Flask(__name__)
 client = Responder()
+db = Memory()
 
 @app.route('/')
 def home():
@@ -11,7 +13,10 @@ def home():
 @app.route('/get_response', methods=['POST'])
 def get_response():
     user_input = request.form['user_input']
-    response = client.get_completion(user_input)
+    memory = db.read_from_memory()
+    memory = '---'.join(mem[0] for mem in memory)
+    response = client.get_completion(user_input, memory)
+    db.write_to_memory(user_input, response)
     return jsonify({"response": response})
 
 if __name__ == '__main__':
