bac9df1ecd451e7372b17808bff9f53945c277e0
olestrausss <o@olestrauss.de>
2024-01-19 19:51:10 -0500

    Fixed gpt api to include memory

--- gpt_helper.py
+++ gpt_helper.py
@@ -7,13 +7,16 @@ load_dotenv()
 class Responder():
     def __init__(self):
         self.client = OpenAI()
+        self.time_limit = os.getenv('TIME_LIMIT_DAYS')
     
-    def get_completion(self, prompt):
+    def get_completion(self, prompt, memory=''):
         completion = self.client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model='gpt-3.5-turbo',
             messages=[
-                {"role": "system", "content": "You are a fork of GPT. If asked who you are, say 'I am a fork of GPT made by Ole S.'"},
-                {"role": "user", "content": prompt}
+                {'role': 'system', 'content': 'You are a fork of GPT. If asked who you are, say "I am a fork of GPT made by Ole S."'},
+                {'role': 'assistant', 'content': memory},
+                {'role': 'user', 'content': prompt}
+                
             ]
         )
         return completion.choices[0].message.content
